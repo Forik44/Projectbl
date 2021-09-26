1ddf35424ff682361e94cbbf3473a8b54f4b1574
Forik <egor.prokofev.ru@gmail.com>
2021-09-26 14:48:37 +0300

    Tower added

--- Source/Projectbl/Building.h
+++ Source/Projectbl/Building.h
@@ -15,6 +15,16 @@ public:
 	// Sets default values for this actor's properties
 	ABuilding();
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Building")
+	int Cost;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Building")
+	FText Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Building")
+	UStaticMeshComponent* Mesh;
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
