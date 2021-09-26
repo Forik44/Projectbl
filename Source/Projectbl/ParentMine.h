4a3cb61f80a7584f0c32db86def02e2b72cfd534
tottttttt <andrey.pukaev@yandex.ru>
2021-09-26 15:23:48 +0300

    Дабавил шахту

---
+++ Source/Projectbl/ParentMine.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Building.h"
+#include "ParentMine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBL_API AParentMine : public ABuilding
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Projectbl/UltimaMine.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ParentMine.h"
+#include "UltimaMine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBL_API AUltimaMine : public AParentMine
+{
+	GENERATED_BODY()
+	
+};
